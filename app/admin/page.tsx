97524f5822490de3c7c7772bed33255aadfb69db
wilfredmanyara <manyarawilly46@gmail.com>
2024-11-29 15:39:08 +0300

    feat(shop-yangu-admin): redirect to dashboard page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import { redirect } from 'next/navigation';
 
-function page() {
-  return (
-    <div>page</div>
-  )
+export default function Home() {
+  redirect('/admin/dashboard');
+
+  return null;
 }
-
-export default page
